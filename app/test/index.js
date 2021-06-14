35258cd696f963fa8357cdd32943c298b75da898
ITasahobby <itasahobby@gmail.com>
2021-06-14 14:04:06 -0400

    Created basic testing when getting cities

--- app/test/index.js
+++ app/test/index.js
@@ -6,6 +6,8 @@ var app = require('../index');
 // Configure Chai
 chai.use(chaiHttp)
 chai.should()
+// Fake DB
+const cities = require('../db/cities.json')
 
 // Unit tests
 describe('Homepage', () => {
@@ -17,5 +19,36 @@ describe('Homepage', () => {
         response.text.should.equal("Hello World!")
         done()
       })
-  })
+  });
+})
+
+// City API
+describe('Testing city API', () => {
+  it('should return the list of countries', done => {
+    chai
+      .request(app)
+      .get('/api/city')
+      .end((error, response) => {
+        response.text.should.equal(JSON.stringify(cities))
+        done()
+      })
+  });
+  it('should return the first country', done => {
+    chai
+      .request(app)
+      .get('/api/city/0')
+      .end((error, response) => {
+        response.text.should.equal(JSON.stringify(cities[0]))
+        done()
+      })
+  });
+  it('should return error when accessing wrong city', done => {
+    chai
+      .request(app)
+      .get('/api/city/99')
+      .end((error, response) => {
+        response.should.status(404)
+        done()
+      })
+  });
 })

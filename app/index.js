af37bda57e5daa6ee699d72900b61488401a3522
ITasahobby <itasahobby@gmail.com>
2021-06-14 13:39:52 -0400

    Created basic functionality

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+// Middlewares
+app.use(bodyParser.json())
+
+// Routes
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use("/api", require("./routes/api"));
 
 app.listen(port, () => {
   console.log(`Sample app listening at http://localhost:${port}`)

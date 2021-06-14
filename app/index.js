fcf7b8916dbada4e62e6336f2a750c7806be99d4
ITasahobby <itasahobby@gmail.com>
2021-06-14 14:08:07 -0400

    Fixed conflicts from previouse merge

--- app/index.js
+++ app/index.js
@@ -11,11 +11,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-<<<<<<< HEAD
 app.use("/api", require("./routes/api"));
-=======
-
->>>>>>> 781d1e20aca43550b7c6b2a890816d04ff1b7471
 
 // Running server
 if (process.env.NODE_ENV === 'test') {
@@ -24,8 +20,4 @@ if (process.env.NODE_ENV === 'test') {
 	app.listen(port, () => {
         console.log(`Sample app listening at http://localhost:${port}`)
     })
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 781d1e20aca43550b7c6b2a890816d04ff1b7471
